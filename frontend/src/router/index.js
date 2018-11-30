ed1a59e80fa6955591ae62cbfb2e772f0f3019e0
Tomer <shaolintl@gmail.com>
2018-11-30 22:27:34 +0100

    renamed backend and created vue frontend

---
+++ frontend/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Dashboard from '@/components/Dashboard'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
+    }
+  ]
+})
