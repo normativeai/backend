8512936a9630ee64d9577756ae218a04ceab31be
Tomer <shaolintl@gmail.com>
2018-12-16 18:11:24 +0100

    moved to tokens - forgot a file

---
+++ app/secureRouter.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var theoryController = require('./controllers/theoryController')
+var userController = require('./controllers/userController')
+
+router.route("/users")
+	.get(userController.user)
+
+router.route("/theories")
+	.get(theoryController.get)
+	.post(theoryController.create)
+
+router.route('/theories/:theoryId')
+  .get(theoryController.getOne)
+  .put(theoryController.update)
+
+module.exports = router;
