135d4956ac158ebd7684088a0dafc596ef29235e
Tomer <shaolintl@gmail.com>
2018-12-21 22:16:05 +0100

    added a ui environment

--- config/ui.js
+++ config/ui.js
@@ -3,5 +3,5 @@
 module.exports = {
   env: 'ui',
   db: 'mongodb://test1111:test1111@ds113799.mlab.com:13799/normativeai-gui',
-  port: process.env.PORT || 300,
+  port: process.env.PORT || 3000,
 };
