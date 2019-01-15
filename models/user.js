510d06bfad47160e9ae32c435a89ef9941a70635
Tomer <shaolintl@gmail.com>
2019-01-15 13:47:20 +0100

    changed password encryption

--- models/user.js
+++ models/user.js
@@ -21,13 +21,18 @@ var userSchema = new Schema({
 userSchema.pre('save', async function(next){
   //'this' refers to the current document about to be saved
   const user = this;
-  //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
-  //your application becomes.
-  const hash = await bcrypt.hash(this.password, 10);
-  //Replace the plain text password with the hash and then store it
-  this.password = hash;
+
+  if (user.isModified("password") || user.isNew) {
+    //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
+    //your application becomes.
+    const hash = await bcrypt.hash(this.password, 10);
+    //Replace the plain text password with the hash and then store it
+    this.password = hash;
   //Indicates we're done and moves on to the next middleware
-  next();
+    next();
+  } else {
+    return next();
+  }
 });
 
 //We'll use this later on to make sure that the user trying to log in has the correct credentials
