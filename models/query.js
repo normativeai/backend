aafbd1a2164ac38d13e24b0a30d4faba9acaf62c
Tomer <shaolintl@gmail.com>
2019-04-29 21:02:59 +0200

    added dirty bit monitoring for avoiding running the prover when no change happens

--- models/query.js
+++ models/query.js
@@ -1,4 +1,5 @@
 //Require Mongoose
+var logger = require('../config/winston');
 var mongoose = require('mongoose');
 
 //Define a schema
@@ -12,36 +13,62 @@ var querySchema = new Schema({
     description       : String,
 		cached_result			: String,
 		theory						: { type: Schema.Types.ObjectId, ref: 'Theory' },
-		user 							: { type: Schema.Types.ObjectId, ref: 'User' }
+		user 							: { type: Schema.Types.ObjectId, ref: 'User' },
+    lastQueryDate     : Date,
+    lastQueryTheorem  : String,
+    lastQueryProof    : String,
+    lastConsistencyDate : Date,
+    lastConsistency   : Boolean
 });
 
 querySchema.methods.execQuery = function(cb) {
-	// in case query is not in cache and cache is not invalidated
-  var helper = require('./queryHelper');
+  if (typeof this.lastQueryDate === 'undefined' || this.lastUpdate > this.lastQueryDate) {
+    var helper = require('./queryHelper');
 
-  if (!!!this.theory) {
-    cb(false, 'Query is not associated with a specific theory. Please set the theory before trying to execute queries');
-  } else if (!this.goal) {
-    cb(false, 'Query has no goal. Please assign goals before trying to execute queries');
+    if (!!!this.theory) {
+      cb(false, 'Query is not associated with a specific theory. Please set the theory before trying to execute queries');
+    } else if (!this.goal) {
+      cb(false, 'Query has no goal. Please assign goals before trying to execute queries');
+    } else {
+      var obj = this
+      helper.executeQuery(this.theory.formalization, this.assumptions, this.goal, function(theorem, proof) {
+        obj.lastQueryTheorem = theorem;
+        obj.lastQueryProof = proof;
+        obj.lastQueryDate = new Date();
+        obj.save(function (err) {
+          logger.error(`Cannot save query state. ${err}`);
+        });
+        cb(theorem, proof);
+      });
+    }
   } else {
-    helper.executeQuery(this.theory.formalization, this.assumptions, this.goal, cb);
+    cb(this.lastQueryTheorem, this.lastQueryProof);
   }
 };
 
 querySchema.methods.isConsistent = function(cb) {
-	// in case the query is not dirty
-  var helper = require('./queryHelper');
+  if (typeof this.lastConsistencyDate === 'undefined' || this.lastUpdate > this.lastConsistencyDate) {
+    var helper = require('./queryHelper');
 
-  if (!!!this.theory) {
-    cb(false, 'Query is not associated with a specific theory. Please set the theory before trying to check for consistency');
+    if (!!!this.theory) {
+      cb(false, 'Query is not associated with a specific theory. Please set the theory before trying to check for consistency');
+    } else {
+      var obj = this
+      helper.executeQuery(this.theory.formalization, this.assumptions, "(x, (~ x))", function(theorem, proof) {
+        if (theorem) {
+        obj.lastConsistency = (theorem != 'Theorem');
+        obj.lastConsistencyDate = new Date();
+        obj.save(function (err) {
+          logger.error(`Cannot save consistency state. ${err}`);
+        });
+          cb(1, theorem != 'Theorem');
+        } else {
+          cb(0, proof);
+        }
+      });
+    }
   } else {
-    helper.executeQuery(this.theory.formalization, this.assumptions, "(x, (~ x))", function(theorem, proof) {
-      if (theorem) {
-        cb(1, theorem != 'Theorem');
-      } else {
-        cb(0, proof);
-      }
-    });
+    cb(1, this.lastConsistency);
   }
 };
 
--- models/theory.js
+++ models/theory.js
@@ -1,4 +1,5 @@
 //Require Mongoose
+var logger = require('../config/winston');
 var mongoose = require('mongoose');
 
 //Define a schema
@@ -10,9 +11,11 @@ var theorySchema = new Schema({
     description       : String,
     content           : String,
     vocabulary        : [{symbol: String, original: String}],
-    formalization     : [{original: String, formula: String, active: Boolean}],
+    formalization     : [{original: String, formula: String, active: Boolean, lastIndependent: Boolean, lastIndependentDate: Date}],
 		user 							: { type: Schema.Types.ObjectId, ref: 'User' },
-    clonedForm        : { type: Schema.Types.ObjectId, ref: 'Theory' }
+    clonedForm        : { type: Schema.Types.ObjectId, ref: 'Theory' },
+    lastConsistency   : Boolean,
+    lastConsistencyDate: Date
 });
 
 theorySchema.statics.isActive = function(form) {
@@ -28,29 +31,47 @@ theorySchema.methods.formalizationAsString = function(possibleFurtherAssumtions)
 };
 
 theorySchema.methods.isConsistent = function(cb) {
-	// in case the theory is not dirty
-  var helper = require('./queryHelper');
-  helper.executeQuery(this.formalization, [], "(x, (~ x))", function(theorem, proof) {
-    if (theorem) {
-      cb(1, theorem != 'Theorem');
-    } else {
-      cb(0, proof);
-    }
-  });
+  if (typeof this.lastConsistencyDate === 'undefined' || this.lastUpdate > this.lastConsistencyDate) {
+    var helper = require('./queryHelper');
+    var obj = this
+    helper.executeQuery(this.formalization, [], "(x, (~ x))", function(theorem, proof) {
+      if (theorem) {
+        obj.lastConsistency = (theorem != 'Theorem');
+        obj.lastConsistencyDate = new Date();
+        obj.save(function (err) {
+          logger.error(`Cannot save consistency state. ${err}`);
+        });
+        cb(1, theorem != 'Theorem');
+      } else {
+        cb(0, proof);
+      }
+    });
+  } else {
+    cb(1, this.lastConsistency);
+  }
 };
 
 theorySchema.methods.isIndependent = function(id, cb) {
   form = this.formalization.id(id);
-  forms = this.formalization.slice(0)
-  forms.splice(this.formalization.indexOf(form), 1)
-  var helper = require('./queryHelper');
-  helper.executeQuery(forms, [], form.formula, function(theorem, proof) {
-    if (theorem) {
-      cb(1, theorem != 'Theorem');
-    } else {
-      cb(0, proof);
-    }
-  });
+  if (typeof form.lastIndependentiDate === 'undefined' || this.lastUpdate > form.lastIndependentDate) {
+    forms = this.formalization.slice(0)
+    forms.splice(this.formalization.indexOf(form), 1)
+    var helper = require('./queryHelper');
+    helper.executeQuery(forms, [], form.formula, function(theorem, proof) {
+      if (theorem) {
+        form.lastIndependent = (theorem != 'Theorem');
+        form.lastIndependentDate = new Date();
+        form.parent().save(function (err) {
+          logger.error(`Cannot save independence state. ${err}`);
+        });
+        cb(1, theorem != 'Theorem');
+      } else {
+        cb(0, proof);
+      }
+    });
+  } else {
+    cb(1,form.lastIndependent);
+  }
 };
 
 module.exports = mongoose.model('Theory', theorySchema );
